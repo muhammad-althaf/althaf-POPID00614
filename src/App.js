9d6258914f9aaf19a21a6e90f93b03d51d6f3080
muhammad-althaf <141054565+muhammad-althaf@users.noreply.github.com>
2023-10-06 19:49:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FileUpload from './FileUplode';
+
+function App() {
+  return (
+    <div className="App">
+     <FileUpload/>
+    </div>
+  );
+}
+
+export default App;
